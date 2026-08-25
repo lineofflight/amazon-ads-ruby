f6c04ea67bae9197a12b790a76ae008d3c86e6ac
lineoffligbot <4628864+lineoffligbot@users.noreply.github.com>
2026-08-25 20:13:21 +0200

    Record real 400 for bare-string filter

--- test/amazon_ads/apis/test_campaigns.rb
+++ test/amazon_ads/apis/test_campaigns.rb
@@ -12,4 +12,18 @@ class TestCampaigns < Minitest::Test
 
     assert_predicate(res.status, :success?)
   end
+
+  def test_query_campaign_rejects_bare_string_filter
+    api = AmazonAds::Campaigns.new(
+      region: "EU",
+      profile_id: ENV.fetch("AMAZON_ADS_TEST_EU_PROFILE_ID", "0"),
+      access_token:,
+    )
+    error = assert_raises(HTTP::StatusError) do
+      api.query_campaign(ad_product_filter: "SPONSORED_PRODUCTS")
+    end
+
+    assert_equal(400, error.response.status.code)
+    assert(error.response.body.to_s.length.positive?)
+  end
 end
